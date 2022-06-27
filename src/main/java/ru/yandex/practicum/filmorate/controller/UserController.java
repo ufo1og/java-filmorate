99389ebb7657c3391d615a62aea374e320a3f453
Андрей Скоробогатов <ufo1og@yandex.ru>
2022-06-27 14:14:11 +0500

    #8 Add controllers users and films

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,52 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private int id = 1;
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @PostMapping
+    public User postUser(@RequestBody @Valid User user) {
+        if ("".equals(user.getName()) || user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        user.setId(id++);
+        log.debug("Добавлен пользователь: {}", user);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User putUser(@RequestBody @Valid User user) {
+        if ("".equals(user.getName()) || user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        if (user.getId() == 0) {
+            user.setId(id++); // Если id не передан в теле запроса - присваиваем свой id
+            log.debug("Добавлен пользователь: {}", user);
+        }
+        if (users.containsKey(user.getId())) {
+            log.debug("Пользователь {} изменен на {}", users.get(user.getId()), user);
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/utility/UserValidator.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.utility;
+
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.utility.exceptions.ValidationException;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+
+public class UserValidator implements ConstraintValidator<ValidUser, User> {
+    private LocalDate now;
+    private Pattern validEmailRegex;
+
+    @Override
+    public void initialize(ValidUser constraintAnnotation) {
+        now = LocalDate.now();
+        validEmailRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    }
+
+    @Override
+    public boolean isValid(User user, ConstraintValidatorContext constraintValidatorContext) {
+        boolean isValid = true;
+        if (user.getId() < 0) {
+            throw new ValidationException("User id can't be less than 0!");
+        }
+        if (user.getEmail() == null || "".equals(user.getEmail()) || !emailValidation(user.getEmail())) {
+            isValid = false;
+        }
+        if (user.getLogin() == null || "".equals(user.getLogin()) || user.getLogin().contains(" ")) {
+            isValid = false;
+        }
+        if (user.getBirthday() == null || !user.getBirthday().isBefore(now)) {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    private boolean emailValidation(String email) {
+        return validEmailRegex.matcher(email).find();
+    }
+}
